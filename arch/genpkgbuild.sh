67fa8758be45a34e0af9d14749833f8a97f6e6c9
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-10-28 18:54:34 +0000

    Try fix genpkgbuild.sh

--- arch/genpkgbuild.sh
+++ arch/genpkgbuild.sh
@@ -2,7 +2,12 @@
 set -e
 set -o pipefail
 
-binary_checksum="$(sha256sum -sd target/release/modman | awk '{print $1}')"
+sha256sum target/release/modman
+binary_checksum="$(sha256sum target/release/modman | awk '{print $1}')"
+echo "Binary Checksum: $binary_checksum"
+
+sha256sum LICENSE
 license_checksum="$(sha256sum LICENSE | awk '{print $1}')"
+echo "License Checksum: $license_checksum"
 
 sed -e "s/\\(pkgver=\\)/\\1$TRAVIS_TAG/" -e "s/\\(sha256sums=\\)/\\1(\\n    '$binary_checksum'\\n    '$license_checksum'\\n)/" "arch/PKGBUILD.proto" > PKGBUILD
