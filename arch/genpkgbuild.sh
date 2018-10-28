935167fa9aaf31dbb091462e9024954b2aa71d17
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-10-28 01:43:29 +0000

    Make PKGBUILD generation script fail on errors

--- arch/genpkgbuild.sh
+++ arch/genpkgbuild.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-binary_checksum="$(sha256sum target/release/modman | awk '{print $1}')"
+set -e
+set -o pipefail
+
+binary_checksum="$(sha256sum -sd target/release/modman | awk '{print $1}')"
 license_checksum="$(sha256sum LICENSE | awk '{print $1}')"
 
 sed -e "s/\\(pkgver=\\)/\\1$TRAVIS_TAG/" -e "s/\\(sha256sums=\\)/\\1(\\n    '$binary_checksum'\\n    '$license_checksum'\\n)/" "arch/PKGBUILD.proto" > PKGBUILD
