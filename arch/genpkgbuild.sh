9d03ea608fecd3cc1da0bed80755570487c59857
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-10-28 01:40:22 +0000

    Remove unnecessary -z flag which broke deployment

--- arch/genpkgbuild.sh
+++ arch/genpkgbuild.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-binary_checksum="$(sha256sum -z target/release/modman | awk '{print $1}')"
-license_checksum="$(sha256sum -z LICENSE | awk '{print $1}')"
+binary_checksum="$(sha256sum target/release/modman | awk '{print $1}')"
+license_checksum="$(sha256sum LICENSE | awk '{print $1}')"
 
 sed -e "s/\\(pkgver=\\)/\\1$TRAVIS_TAG/" -e "s/\\(sha256sums=\\)/\\1(\\n    '$binary_checksum'\\n    '$license_checksum'\\n)/" "arch/PKGBUILD.proto" > PKGBUILD
