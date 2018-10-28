d375ee0c806166e46fa879365f2386d8d2d37ce0
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-10-28 01:29:59 +0000

    Add PKGBUILD generation script

---
+++ arch/genpkgbuild.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+binary_checksum="$(sha256sum -z target/release/modman | awk '{print $1}')"
+license_checksum="$(sha256sum -z LICENSE | awk '{print $1}')"
+
+sed -e "s/\\(pkgver=\\)/\\1$TRAVIS_TAG/" -e "s/\\(sha256sums=\\)/\\1(\\n    '$binary_checksum'\\n    '$license_checksum'\\n)/" "arch/PKGBUILD.proto" > PKGBUILD
